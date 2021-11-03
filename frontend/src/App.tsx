550bb3496fc59f666c9a25ddc5922bb8c852377f
Andre Nunes <andre.totslaw@gmail.com>
2021-11-02 23:10:35 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import 'components/NavBar/index.tsx';
 import NavBar from 'components/NavBar';
 import DataTable from 'components/DataTable';
+import BarChart from 'components/BarChart';
+import DonutChart from 'components/DonutChart';
 import Footer from 'components/Footer';
 
 function App() {
@@ -9,7 +11,23 @@ function App() {
     <> 
       <NavBar />
       <div className="container">
-        <h1 className="text-primary">Hello World!</h1>
+        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
+            <BarChart />
+          </div>  
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Todas as vendas</h5>
+            <DonutChart />
+          </div>  
+        </div>
+
+        <div className="py-5">
+          <h2 className="text-primary">Todas as vendas</h2>
+        </div>
+
         <DataTable />
     </div>
       <Footer />
--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -23,28 +23,28 @@ const DataTable = () => {
                 </tr>
                 <tr>
                     <td>22/04/2021</td>
-                    <td>Barry Allen</td>
-                    <td>34</td>
-                    <td>25</td>
-                    <td>15017.00</td>
+                    <td>Bruce Wayne</td>
+                    <td>24</td>
+                    <td>35</td>
+                    <td>16500.00</td>
                 </tr>                <tr>
                     <td>22/04/2021</td>
-                    <td>Barry Allen</td>
-                    <td>34</td>
-                    <td>25</td>
-                    <td>15017.00</td>
+                    <td>Clack Kent</td>
+                    <td>40</td>
+                    <td>22</td>
+                    <td>22015.00</td>
                 </tr>                <tr>
-                    <td>22/04/2021</td>
-                    <td>Barry Allen</td>
-                    <td>34</td>
-                    <td>25</td>
-                    <td>15017.00</td>
+                    <td>25/05/2021</td>
+                    <td>Hal Jordan</td>
+                    <td>15</td>
+                    <td>28</td>
+                    <td>18100.00</td>
                 </tr>                <tr>
-                    <td>22/04/2021</td>
-                    <td>Barry Allen</td>
-                    <td>34</td>
-                    <td>25</td>
-                    <td>15017.00</td>
+                    <td>28/05/2021</td>
+                    <td>ALan Scott</td>
+                    <td>30</td>
+                    <td>20</td>
+                    <td>35045.00</td>
                 </tr>                <tr>
                     <td>22/04/2021</td>
                     <td>Barry Allen</td>
