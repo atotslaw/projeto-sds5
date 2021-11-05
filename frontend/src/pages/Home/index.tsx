3f8fe7c63974347446d8421038dd3e26804c0a46
Andre Nunes <andre.totslaw@gmail.com>
2021-11-05 10:39:30 -0300

    Home, navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
-        <h2>Página Home</h2> 
-    </div>
-    );
-}
+    <>
+      <NavBar />
+      <div className="container">
+        <div className="jumbotron">
+          <h1 className="display-4">DSVendas</h1>
+          <p className="lead">
+            Analise o desempenho das vendas por diferentes perspectivas
+          </p>
+          <hr />
+          <p>
+            Esta aplicação exibe um Dashboard baseado nas informações de vendas
+            obtidas via back end em Spring Boot.
+          </p>
+          <Link className="btn btn-primary btn-lg" to="/dashboard">
+              Acessar Dashboard
+          </Link>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
 export default Home;
